8c59cf26e1fa36cb266e5434b3dacff4f06cf489
Shreevathsa05 <bhatshreevathsa17@gmail.com>
2025-12-29 16:50:31 +0530

    first commit

---
+++ ui-library-package/components/buttons/BasicButton.jsx
@@ -0,0 +1,12 @@
+import '../../index.css'
+import React from 'react'
+
+function BasicButton() {
+  return (
+    <div className='bg-gray-700 min-h-screen min-w-screen'>
+      hi
+    </div>
+  )
+}
+
+export default BasicButton
