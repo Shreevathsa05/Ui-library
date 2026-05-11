e2fd7ecae3aa87a99e4d59e162b27910dd24ab86
Parth-Gupta05 <parthgupta20052005@gmail.com>
2026-05-11 21:19:55 +0530

    fixes

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,10 +1,24 @@
+// import { BasicButton, BasicCarousel } from '../../ui-library-package'
+import { BasicCarousel } from '../../ui-library-package/components/carousel/BasicCarousel'
+
 import './index.css'
 import React from 'react'
 function TestingRig() {
   return (
-    <div className='bg-gray-700 min-h-screen min-w-screen'>
-      hi
+    <>
+    <div className='bg-blue-950 w-screen h-screen'>
+    <BasicCarousel
+  className=" h-[90vh]"
+  slideClassName="border-2 border-white shadow-2xl"
+  items={[
+    <div className="bg-indigo-500 w-full h-full rounded-xl" />,
+    <div className="bg-pink-500 w-full h-full rounded-xl" />,
+    <div className="bg-orange-500 w-full h-full rounded-xl" />,
+  ]}
+/>
+
     </div>
+    </>
   )
 }
 
